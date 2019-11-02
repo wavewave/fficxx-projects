855991b4b9e3176fa5ba37831525c0d70f12b415
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 15:34:49 -0700

    add OGDF.

--- release.nix
+++ release.nix
@@ -3,6 +3,8 @@
 with pkgs;
 
 let
+  # TODO: should be packaged into the upstream nixpkgs.
+  ogdf = callPackage ./hs-ogdf/ogdf/default.nix {};
 
   fficxxSrc = lib.cleanSource ./fficxx;
 
@@ -25,16 +27,20 @@ let
         "fficxx"         = self.callCabal2nix "fficxx"         (fficxxSrc + "/fficxx")         {};
         "stdcxx"         = self.callPackage stdcxxNix {};
       }
-      // (import ./HROOT { inherit pkgs fficxxSrc; } self super)
-      // (import ./hgdal { inherit pkgs fficxxSrc; } self super);
+      // (import ./HROOT   { inherit pkgs fficxxSrc; } self super)
+      // (import ./hgdal   { inherit pkgs fficxxSrc; } self super)
+      // (import ./hs-ogdf { inherit pkgs fficxxSrc ogdf; } self super);
   };
 
 in
 
 {
+
   "fficxx"         = newHaskellPackages.fficxx;
   "fficxx-runtime" = newHaskellPackages.fficxx-runtime;
-  "stdcxx"         = newHaskellPackages.stdcxx
+  "stdcxx"         = newHaskellPackages.stdcxx;
   "HROOT"          = newHaskellPackages.HROOT;
   "hgdal"          = newHaskellPackages.hgdal;
+  "OGDF"           = newHaskellPackages.OGDF;
+
 }
