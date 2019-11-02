1d96402fb8aefa1e1828ecae8e23b11231fae017
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 13:18:10 -0700

    add hgdal in release.nix

--- release.nix
+++ release.nix
@@ -7,11 +7,14 @@ let
   fficxxSrc = lib.cleanSource ./fficxx;
 
   newHaskellPackages = haskellPackages.override {
-    overrides = import ./HROOT { inherit pkgs fficxxSrc; };
+    overrides = self: super:
+         (import ./HROOT { inherit pkgs fficxxSrc; } self super)
+      // (import ./hgdal { inherit pkgs fficxxSrc; } self super);
   };
 
 in
 
 {
   "HROOT" = newHaskellPackages.HROOT;
+  "hgdal" = newHaskellPackages.hgdal;
 }
