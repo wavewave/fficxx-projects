86db3151b29d038fa0c291ecedcb9bde2511cdb8
Ian-Woo Kim <ianwookim@gmail.com>
2019-11-02 13:22:35 -0700

    add correct dependencies (stdcxx)

--- release.nix
+++ release.nix
@@ -6,15 +6,35 @@ let
 
   fficxxSrc = lib.cleanSource ./fficxx;
 
+  newHaskellPackages0 = haskellPackages.override {
+    overrides = self: super: {
+      "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" (fficxxSrc + "/fficxx-runtime") {};
+      "fficxx"         = self.callCabal2nix "fficxx"         (fficxxSrc + "/fficxx")         {};
+    };
+  };
+
+  stdcxxNix = import (fficxxSrc + "/stdcxx-gen/default.nix") {
+    inherit stdenv;
+    haskellPackages = newHaskellPackages0;
+  };
+
   newHaskellPackages = haskellPackages.override {
     overrides = self: super:
-         (import ./HROOT { inherit pkgs fficxxSrc; } self super)
+      {
+        "fficxx-runtime" = self.callCabal2nix "fficxx-runtime" (fficxxSrc + "/fficxx-runtime") {};
+        "fficxx"         = self.callCabal2nix "fficxx"         (fficxxSrc + "/fficxx")         {};
+        "stdcxx"         = self.callPackage stdcxxNix {};
+      }
+      // (import ./HROOT { inherit pkgs fficxxSrc; } self super)
       // (import ./hgdal { inherit pkgs fficxxSrc; } self super);
   };
 
 in
 
 {
-  "HROOT" = newHaskellPackages.HROOT;
-  "hgdal" = newHaskellPackages.hgdal;
+  "fficxx"         = newHaskellPackages.fficxx;
+  "fficxx-runtime" = newHaskellPackages.fficxx-runtime;
+  "stdcxx"         = newHaskellPackages.stdcxx
+  "HROOT"          = newHaskellPackages.HROOT;
+  "hgdal"          = newHaskellPackages.hgdal;
 }
